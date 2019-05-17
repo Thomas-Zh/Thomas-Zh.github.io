65ea03f82d125e8337e6c1fe7b94cd41cbf83f83
thomaszh1998@gmail.com <thomaszh1998@gmail.com>
2019-05-16 20:00:23 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route } from "react-router-dom";
 import ShowPost from "./components/showpost.component";
 import EditPost from "./components/editpost.component";
 import CreatePost from "./components/createpost.component";
-import { HashRouter, Route } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <HashRouter basename='/'>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="Container">
       <h2>lol</h2>
       </div>
       <Route path="/posts" component={ShowPost}/>
       <Route path="/posts/create" component={CreatePost}/>
       <Route path="/posts/edit" component={EditPost}/>
-      </HashRouter>
+      </BrowserRouter>
     );
   }
 }
